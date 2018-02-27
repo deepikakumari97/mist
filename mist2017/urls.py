7bbeb16fa77f83d38c9b14f965fbb743a9c2eacb
Deepika Kumari <deepika.dk97@gmail.com>
2018-02-27 05:43:20 +0530

    Added urls.py

---
+++ mist2017/urls.py
@@ -0,0 +1,32 @@
+"""mist2017 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from login.views import FacebookLogin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^vanilla/', admin.site.urls),
+    url(r'^$', include('login.urls')),
+    url(r'^policy', include('policy.urls')),
+    url(r'^leaderboard/', include('player.urls')),
+    url(r'^dashboard/', include('question.urls')),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
